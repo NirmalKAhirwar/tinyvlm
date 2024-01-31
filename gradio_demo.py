9d2747ac3da53a5258cf41fb3afdaa70869f93ce
Nirmal Kumar Ahirwar <ahirwarnirmal2017@gmail.com>
2024-01-31 21:15:26 +0530

    2nd commit

--- gradio_demo.py
+++ gradio_demo.py
@@ -18,6 +18,7 @@ moondream.eval()
 
 
 def answer_question(img, prompt):
+   
     image_embeds = moondream.encode_image(img)
     streamer = TextIteratorStreamer(tokenizer, skip_special_tokens=True)
     thread = Thread(
@@ -36,22 +37,30 @@ def answer_question(img, prompt):
         clean_text = re.sub("<$|END$", "", new_text)
         buffer += clean_text
         yield buffer.strip("<END")
+        # print("buffer: ", buffer)
 
 
 with gr.Blocks() as demo:
     gr.Markdown(
         """
-        # 🌔 moondream
-        ### A tiny vision language model. [GitHub](https://github.com/vikhyat/moondream)
+        # 🌔 TinyVLM -- Chat with Images!
+        ### A tiny vision language model. [GitHub](https://github.com/NirmalKAhirwar/tinyvlm.git)
         """
     )
     with gr.Row():
         prompt = gr.Textbox(label="Input Prompt", placeholder="Type here...", scale=4)
         submit = gr.Button("Submit")
+        
     with gr.Row():
         img = gr.Image(type="pil", label="Upload an Image")
         output = gr.TextArea(label="Response", info="Please wait for a few seconds..")
+        
+        # audio_btn = gr.Audio(label="Listen", type="file", source="empty.mp3")
+        
+        
     submit.click(answer_question, [img, prompt], output)
+    print("Outputs: ", answer_question)
     prompt.submit(answer_question, [img, prompt], output)
+    print("Outputs: ", answer_question)
 
-demo.queue().launch(debug=True)
+demo.queue().launch(debug=True, share=True)
